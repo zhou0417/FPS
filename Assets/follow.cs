fb79ed60cc5038fb3f67a0cc7924cb7dae733f22
Zhou Yuan <1693081@CEGEPGIM.LCL>
2017-08-29 13:15:56 -0400

    second commit

---
+++ Assets/follow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour {
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(player);
+        transform.Translate(Vector3.forward * Time.deltaTime);
+	}
+}
---
+++ Assets/shoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+    public GameObject bulletSpawn;
+    public GameObject bullet;
+    float countdown = -1;
+	// Use this for initialization
+	void Start () {
+        bulletSpawn = GameObject.FindWithTag("GameObject");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countdown = countdown - Time.deltaTime;
+        if (countdown < 0)
+        {
+            countdown = 0.5f;
+            GameObject obj = (GameObject)Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            obj.GetComponent<Rigidbody>().velocity = Vector3.back * 20;
+            Destroy(obj, 5.0f);
+        }
+	}
+}
